5fce975ce58b15aafe32648a3b73ae6f27224e05
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-02-17 20:10:54 -0700

    add jquery, modulefile, and samle jedivar.yaml

---
+++ modulefiles/jtools.lua
@@ -0,0 +1,13 @@
+local fn      = myFileName()
+local module  = myModuleFullName()
+local loc     = fn:find(module,1,true)-2
+local mdir    = fn:sub(1,loc)
+local pkg     = pathJoin(mdir, "/jtools")
+
+--LmodMsgRaw("debug-fn:"..fn..'\n')
+--LmodMsgRaw("debug-module:"..module..'\n')
+--LmodMsgRaw("debug-loc:"..loc..'\n')
+--LmodMsgRaw("debug-mdir:"..mdir..'\n')
+--LmodMsgRaw("debug-pkg:"..pkg..'\n')
+
+prepend_path("PATH",pkg)
