7eaaaf154e88523aa98c49648f312e8f446d95a4
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-03-02 15:30:57 -0700

    make yaml_cat_together.py flexible for different combination of observers

--- misc/yaml_cat_together.py
+++ misc/yaml_cat_together.py
@@ -1,46 +1,46 @@
 #!/usr/bin/env python
-import os
+import os, sys
 # list of available observers
 dcObserver={
-"t133": "aircar_airTemperature_133",
-"q133": "aircar_specificHumidity_133",
-"uv233": "aircar_winds_233",
-"t130": "aircft_airTemperature_130",
-"t131": "aircft_airTemperature_131",
-"t134": "aircft_airTemperature_134",
-"t135": "aircft_airTemperature_135",
-"q134": "aircft_specificHumidity_134",
-"uv230": "aircft_winds_230",
-"uv231": "aircft_winds_231",
-"uv234": "aircft_winds_234",
-"uv235": "aircft_winds_235",
-"t120": "adpupa_airTemperature_120",
-"q120": "adpupa_specificHumidity_120",
-"ps120": "adpupa_stationPressure_120",
-"uv220": "adpupa_winds_220",
-"t181": "adpsfc_airTemperature_181",
-"t187": "adpsfc_airTemperature_187",
-"q181": "adpsfc_specificHumidity_181",
-"q187": "adpsfc_specificHumidity_187",
-"ps181": "adpsfc_stationPressure_181",
-"ps187": "adpsfc_stationPressure_187",
-"uv281": "adpsfc_winds_281",
-"uv287": "adpsfc_winds_287",
-"amsua_n19": "amsua_n19",
-"amsua_n20": "atms_n20",
-"atms_npp": "atms_npp_qc_bc",
-"atmos_npp": "atms_npp",
-"t188": "msonet_airTemperature_188",
-"q188": "msonet_specificHumidity_188",
-"ps188": "msonet_stationPressure_188",
-"uv288": "msonet_winds_288",
-"uv227": "proflr_winds_227",
-"t126": "rassda_airTemperature_126",
-"t180": "sfcshp_airTemperature_180",
-"q180": "sfcshp_specificHumidity_180",
-"ps180": "sfcshp_stationPressure_180",
-"uv280": "sfcshp_winds_280",
-"uv224": "vadwnd_winds_224",
+  "t133": "aircar_airTemperature_133",
+  "q133": "aircar_specificHumidity_133",
+  "uv233": "aircar_winds_233",
+  "t130": "aircft_airTemperature_130",
+  "t131": "aircft_airTemperature_131",
+  "t134": "aircft_airTemperature_134",
+  "t135": "aircft_airTemperature_135",
+  "q134": "aircft_specificHumidity_134",
+  "uv230": "aircft_winds_230",
+  "uv231": "aircft_winds_231",
+  "uv234": "aircft_winds_234",
+  "uv235": "aircft_winds_235",
+  "t120": "adpupa_airTemperature_120",
+  "q120": "adpupa_specificHumidity_120",
+  "ps120": "adpupa_stationPressure_120",
+  "uv220": "adpupa_winds_220",
+  "t181": "adpsfc_airTemperature_181",
+  "t187": "adpsfc_airTemperature_187",
+  "q181": "adpsfc_specificHumidity_181",
+  "q187": "adpsfc_specificHumidity_187",
+  "ps181": "adpsfc_stationPressure_181",
+  "ps187": "adpsfc_stationPressure_187",
+  "uv281": "adpsfc_winds_281",
+  "uv287": "adpsfc_winds_287",
+  "amsua_n19": "amsua_n19",
+  "amsua_n20": "atms_n20",
+# "atmos_npp": "atms_npp",
+  "atms_npp": "atms_npp_qc_bc",
+  "t188": "msonet_airTemperature_188",
+  "q188": "msonet_specificHumidity_188",
+  "ps188": "msonet_stationPressure_188",
+  "uv288": "msonet_winds_288",
+  "uv227": "proflr_winds_227",
+  "t126": "rassda_airTemperature_126",
+  "t180": "sfcshp_airTemperature_180",
+  "q180": "sfcshp_specificHumidity_180",
+  "ps180": "sfcshp_stationPressure_180",
+  "uv280": "sfcshp_winds_280",
+  "uv224": "vadwnd_winds_224"
     }
 
 # list of header files
@@ -53,9 +53,31 @@ listHeader=[
 #
 # determine dcObserverUser
 #
-dcObserverUser={}
-dcObserverUser['t133']=dcObserver['t133']
+args=sys.argv
+nargs=len(args)-1
+if nargs <1:
+  print(f"{args[0]} [?|query|all|obs_str]\n")
+  print(f"  obs_str specifies a subset of observers, delimited with a comma")
+  print(f"  eg: t133  or t133,t187 or t133,q133,uv233 etc")
+  exit()
 
+if args[1] == "query" or args[1] == "?":
+  summary=""
+  detail=""
+  for key,value in dcObserver.items():
+    summary+=f"{key},"
+    detail+=f"{key}={value}\n"
+  print(f"{detail.rstrip(',')}\n")
+  print(f"{summary.rstrip(',')}\n")
+  exit()
+elif args[1] == "all":
+  obsUser=list(dcObserver.keys())
+else:
+  obsUser=args[1].split(",")
+# create dcObserverUser
+dcObserverUser={}
+for key in obsUser:
+  dcObserverUser[key]=dcObserver[key]
 #
 # write out rrfs-workflow yaml files
 #
@@ -159,3 +181,7 @@ with open("getkf_solver.yaml", 'r') as infile, open(".tmp.solver.yaml", 'w') as 
         outfile.write(line)
 # ~~~~~~~~
 os.replace(".tmp.solver.yaml","getkf_solver.yaml")
+#
+# print out information
+#
+print("jedivar.yaml\ngetkf_observer.yaml\ngetkf_solver.yaml\n\ngenerated under current directory")
