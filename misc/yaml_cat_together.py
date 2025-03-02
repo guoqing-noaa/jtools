ce538481691499ce78144b67261a08dc9a792260
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-03-02 14:18:34 -0700

    add all available observers into the dictionary

--- misc/yaml_cat_together.py
+++ misc/yaml_cat_together.py
@@ -1,22 +1,62 @@
 #!/usr/bin/env python
 import os
-#
+# list of available observers
 dcObserver={
 "t133": "aircar_airTemperature_133",
-
+"q133": "aircar_specificHumidity_133",
+"uv233": "aircar_winds_233",
+"t130": "aircft_airTemperature_130",
+"t131": "aircft_airTemperature_131",
+"t134": "aircft_airTemperature_134",
+"t135": "aircft_airTemperature_135",
+"q134": "aircft_specificHumidity_134",
+"uv230": "aircft_winds_230",
+"uv231": "aircft_winds_231",
+"uv234": "aircft_winds_234",
+"uv235": "aircft_winds_235",
+"t120": "adpupa_airTemperature_120",
+"q120": "adpupa_specificHumidity_120",
+"ps120": "adpupa_stationPressure_120",
+"uv220": "adpupa_winds_220",
+"t181": "adpsfc_airTemperature_181",
+"t187": "adpsfc_airTemperature_187",
+"q181": "adpsfc_specificHumidity_181",
+"q187": "adpsfc_specificHumidity_187",
+"ps181": "adpsfc_stationPressure_181",
+"ps187": "adpsfc_stationPressure_187",
+"uv281": "adpsfc_winds_281",
+"uv287": "adpsfc_winds_287",
+"amsua_n19": "amsua_n19",
+"amsua_n20": "atms_n20",
+"atms_npp": "atms_npp_qc_bc",
+"atmos_npp": "atms_npp",
+"t188": "msonet_airTemperature_188",
+"q188": "msonet_specificHumidity_188",
+"ps188": "msonet_stationPressure_188",
+"uv288": "msonet_winds_288",
+"uv227": "proflr_winds_227",
+"t126": "rassda_airTemperature_126",
+"t180": "sfcshp_airTemperature_180",
+"q180": "sfcshp_specificHumidity_180",
+"ps180": "sfcshp_stationPressure_180",
+"uv280": "sfcshp_winds_280",
+"uv224": "vadwnd_winds_224",
     }
 
-# header
+# list of header files
 listHeader=[
   "basic_config/mpasjedi_en3dvar.yaml",
   "basic_config/mpasjedi_getkf_observer.yaml",
   "basic_config/mpasjedi_getkf_solver.yaml"
     ]
-# ~~~~
+
+#
 # determine dcObserverUser
 #
-dcObserverUser=dcObserver
-# ~~~~
+dcObserverUser={}
+dcObserverUser['t133']=dcObserver['t133']
+
+#
 # write out rrfs-workflow yaml files
 #
 obdir="obtype_config/"
@@ -67,7 +107,11 @@ for fheader in listHeader:
             else:
               line=line.replace("@DISTRIBUTION@","RoundRobin")
           elif value in line:
-            line=line.replace(value,key)
+            if "obsfile" in line:
+              line=line.replace(value,key)
+            elif "name" in line:
+              line=line.replace(value,f'{key}={value}')
+
           # ~~~~~~
           outfile.write(line)
 #
